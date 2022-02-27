dcf147057efc0b569330fd269a5c52c956be83b4
angel <dehainaut.angelique@orange.fr>
2022-02-27 13:42:40 +0100

    end

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
+require 'Connect.php';
+require 'Config.php';
 
+$myConnexion = Connect::dbConnect();
+
+$stmt = $myConnexion->prepare("
+    SELECT user.username, role.role
+    FROM user_role
+    INNER JOIN user ON user_role.user_id = user.id
+    INNER JOIN role ON user_role.role_id = role.id
+");
+
+$stmt->execute();
+
+echo "<pre>";
+print_r($stmt->fetchAll());
+echo "<pre>";
 /*
  * 1 - Uniquement pour la pratique, reproduisez via mysql workbench le schémas proposé.
  * 2 - Exportez le résultat de manière à créer les tables en base de données.
